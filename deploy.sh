e3e5b30541ceea0c07964da5cba86bef98617824
AYUSH K SANTHOSH <gamerudy007@gmail.com>
2024-11-28 21:00:39 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ eksctl create cluster -f cluster.yaml --kubeconfig kubeconfig
 kubectl --kubeconfig ./kubeconfig apply -f ./run-my-nginx.yaml 
 
 # expose the nginx containers
-kubectl --kubeconfig ./kubeconfig expose deployment/my-nginx \
+kubectl --kubeconfig ./kubeconfig expose deployment/demo-nginx \
         --port=80 --target-port=80 \
         --name=my-nginx-service --type=LoadBalancer
 
